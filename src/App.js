c720927508414b786e8bc0770e5870694fe96358
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-06-22 12:26:21 +0200

    added basic layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
-    <div>
-     
-    </div>
+    <Layout>
+      <h1>Main Page</h1>
+      <p>Work in progress...</p>
+    </Layout>
   );
 }
 
