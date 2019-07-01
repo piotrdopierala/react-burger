625b76b4422260cb2282c9a0724054c906e1498d
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-07-01 19:43:36 +0200

    side drawer for small screens

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,17 +2,20 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => {
 
     return (
         <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo />
-            <nav>
-                <NavigationItems />
-            </nav>
-        </header>
+            <DrawerToggle clicked={props.drawerToggleClicked}/>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
+        </header >
     );
 }
 
