f31d8539f530db13c003a212d33ee0a3543b113c
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-09-11 12:29:41 +0200

    blocking routes for non-logged-in scenario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -11,23 +11,42 @@ import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
 class App extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.onTryAutoSingup();
   }
   render() {
-    return (
-      <Layout>
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/' component={BurgerBuilder} />
+        <Redirect to='/'/>
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
         <Switch>
           <Route path='/checkout' component={Checkout} />
           <Route path='/orders' component={Orders} />
-          <Route path='/auth' component={Auth} />
           <Route path='/logout' component={Logout} />
           <Route path='/' component={BurgerBuilder} />
         </Switch>
+      );
+    }
+
+    return (
+      <Layout>
+        {routes}
       </Layout>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -36,4 +55,4 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
